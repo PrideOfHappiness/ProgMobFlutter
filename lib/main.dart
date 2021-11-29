08fb04af20706188cf34784da48e9931ac39d298
Renaldi <soeryadi.renaldi@gmail.com>
2021-11-29 19:39:44 +0700

    Selesai Flutter

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutterdemo/pertemuan1.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Pertemuan1(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void navigateLogin() async{
+      SharedPreferences pref = await SharedPreferences.getInstance();
+
+
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+             RaisedButton(
+              child: const Text(
+                'Login'
+              ),
+              onPressed: () async{
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                await prefs.setInt("is_login", 1);
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const Pertemuan1(title: "Hello!")),
+                );
+              }
+             ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/pertemuan1.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+  class Pertemuan1 extends StatefulWidget {
+  const Pertemuan1({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<Pertemuan1> createState() => _Pertemuan1State();
+}
+
+  class _Pertemuan1State extends State<Pertemuan1> {
+    final _formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Form(
+        key: _formkey,
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+          children: [
+            TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Nama Lengkap",
+                  hintText: "Contoh: Renaldi Soeryadi",
+                    icon: Icon(Icons.people),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    )
+              ),
+                    validator: (value){
+                  if (value!.isEmpty){
+                    return("Nama tidak boleh kosong!");
+                    }
+                  return null;
+                  },
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "Tes Input 2",
+                hintText: "Teks yang akan diinput disini",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                )
+              ),
+            ),
+            RaisedButton(
+              focusColor: Colors.blue,
+              color: Colors.blueGrey,
+              onPressed: () {
+                if(_formkey.currentState!.validate()) {}
+              },
+              child: const Text(
+                "Submit",
+                style: TextStyle(
+                    color: Colors.white
+                ),
+              ),
+            ),
+             RaisedButton(
+              focusColor: Colors.blue,
+              color: Colors.blueGrey,
+               onPressed: () async{
+                 SharedPreferences prefs = await SharedPreferences.getInstance();
+                 await prefs.setInt("is_login", 1);
+                 Navigator.pushReplacement(
+                     context,
+                     MaterialPageRoute(
+                     builder: (context) => const Pertemuan1(title: "Hello!")),
+                 );
+                },
+                child: const Text(
+                  "Logout",
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+            )
+          ],
+        ),
+      ),
+      ),
+    );
+  }
+}
